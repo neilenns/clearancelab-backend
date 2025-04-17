3afbe14036e48a0419652baf01120201bdc448aa
Neil Enns <neile@live.com>
2025-04-17 22:05:56 +0000

    Add summary, get rid of nanoid

--- src/routes/scenarios.ts
+++ src/routes/scenarios.ts
@@ -1,25 +1,41 @@
-import { Router, Request, Response, NextFunction } from "express";
-import { ScenarioModel } from "../models/scenario.js";
+import { NextFunction, Request, Response, Router } from "express";
 import { verifyApiKey } from "../middleware/apikey.js";
+import { ScenarioModel } from "../models/scenario.js";
 
 const router = Router();
 
+interface ScenarioQuery {
+  summary?: string;
+}
+
 router.get(
-  "/scenarios",
+  "/",
   verifyApiKey,
-  async (_req: Request, res: Response, next: NextFunction) => {
+  async (
+    req: Request<unknown, unknown, unknown, ScenarioQuery>,
+    res: Response,
+    next: NextFunction
+  ) => {
     try {
-      const scenarios = await ScenarioModel.findAll();
+      const summary = req.query.summary === "true";
+
+      // Get partial data if summary was requested.
+      const projection = summary
+        ? { title: 1, isValid: 1, "plan.dep": 1, "plan.dest": 1, "plan.aid": 1 }
+        : undefined;
+
+      const scenarios = await ScenarioModel.find({}, projection).lean();
+
       res.json(scenarios);
     } catch (err) {
-      next(err); // Pass to centralized error handler
+      next(err);
     }
   }
 );
 
 // Add a route to look up a scenario by ID
 router.get(
-  "/scenarios/:id",
+  "/:id",
   verifyApiKey,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
